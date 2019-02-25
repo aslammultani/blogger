13999e7d4744e9db47ceb4f724f452abdfff9f43
aslammultidots <aslam.multani@multidots.com>
2019-02-25 12:49:11 +0530

    removed index key

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,12 +1,21 @@
 const config = {
-  apiKey: process.env.GATSBY_API_KEY,
-  authDomain: process.env.GATSBY_AUTH_DOMAIN,
-  databaseURL: process.env.GATSBY_DATABASE_URL,
-  projectId: process.env.GATSBY_PROJECT_ID,
-  storageBucket: process.env.GATSBY_STORAGE_BUCKET,
-  messagingSenderId: process.env.GATSBY_MESSAGING_SENDER_ID,
-};
-console.log("Key:", process.env.GATSBY_API_KEY);
+  apiKey: 'AIzaSyB_EC4kekLVlRJS5Xq3uZXgZEd9_x36PvA',
+  authDomain: 'gcstarter01.firebaseapp.com',
+  databaseURL: 'https://gcstarter01.firebaseio.com',
+  projectId: 'gcstarter01',
+  storageBucket: 'gcstarter01.appspot.com',
+  messagingSenderId: '826368275004',
+}
+
+// const config = {
+//   apiKey: process.env.GATSBY_API_KEY,
+//   authDomain: process.env.GATSBY_AUTH_DOMAIN,
+//   databaseURL: process.env.GATSBY_DATABASE_URL,
+//   projectId: process.env.GATSBY_PROJECT_ID,
+//   storageBucket: process.env.GATSBY_STORAGE_BUCKET,
+//   messagingSenderId: process.env.GATSBY_MESSAGING_SENDER_ID,
+// };
+
 class Firebase {
   constructor(app) {
     app.initializeApp(config)
