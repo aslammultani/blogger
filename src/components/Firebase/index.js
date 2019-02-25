c2ef34aaa64d167a72746f8edb3831ab7da1d42f
aslammultidots <aslam.multani@multidots.com>
2019-02-25 12:27:43 +0530

    edited env vars

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,10 +1,10 @@
 const config = {
   apiKey: process.env.FB_API_KEY,
-  messagingSenderId: process.env.FB_APP_MESSAGING_SENDER_ID,
   authDomain: process.env.FB_AUTH_DOMAIN,
   databaseURL: process.env.FB_DATABASE_URL,
   projectId: process.env.FB_PROJECT_ID,
   storageBucket: process.env.FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
 };
 
 class Firebase {
